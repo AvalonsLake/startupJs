de3decd40fa40252d2c6eecee6dc32357bdc707f
AvalonsLakes <avalonslake@gmail.com>
2025-08-20 11:34:37 -0600

    final Updates

--- Store-API/app.js
+++ Store-API/app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
-const prouductsRouter = require("./routes/products");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -16,10 +16,10 @@ app.use(express.json());
 // routes
 
 app.get("/", (req, res) => {
-  res.send('<h1>Store API</h1><a href="/api/v1/products>products route</a>"');
+  res.send('<h1>Store API</h1><a href="/api/v1/products>Products Route</a>"');
 });
 
-app.use("/api/v1/products", prouductsRouter);
+app.use("/api/v1/products", productsRouter);
 
 // products route
 
@@ -34,7 +34,7 @@ const start = async () => {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
-    console.log(`Your error is... ${error}`);
+    console.log(`the error is ${error}`);
   }
 };
 
